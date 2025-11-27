1271ab0855d966754f12da6ff31c931e3ec5c941
Aniketh Mungara <aniketh.official@gmail.com>
2025-11-26 19:33:32 -0700

    Another Day, Another problem:

---
+++ Arrays/#42_Trapping_Water.py
@@ -0,0 +1,24 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height: return 0
+
+        l, r = 0,len(height)-1
+        leftMax, rightMax= height[l], height[r]
+        res=0
+
+
+        while l < r:
+            if leftMax < rightMax:
+                l+=1
+                leftMax=max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r-=1
+                rightMax= max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Using two pointer approach to calculate the amount of water that can be trapped after raining given the heights of the bars.
