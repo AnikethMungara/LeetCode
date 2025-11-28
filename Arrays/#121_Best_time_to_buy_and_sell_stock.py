42252cd6e16bdd7d6e50a7ffe5a6aadd76f84e53
Aniketh Mungara <aniketh.official@gmail.com>
2025-11-27 20:58:03 -0700

    Happy Thanksgiving!

--- Arrays/#121_Best_time_to_buy_and_sell_stock.py
+++ Arrays/#121_Best_time_to_buy_and_sell_stock.py
@@ -1,3 +1,8 @@
+# Time Complexity: O(n)    
+# Space Complexity: O(1)=
+# Using dynamic programming approach to keep track of the minimum price and maximum profit while iterating through the list of prices.
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
@@ -11,3 +16,24 @@ class Solution:
                 maxprofit = price - minprice
         
         return maxprofit if maxprofit != -inf else 0
+    
+
+
+
+
+#there is another approach using two pointer technique which has the same time and space complexity.
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1
+        maxprofit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxprofit = max(maxprofit, profit)
+            else:
+                left = right
+            right += 1
+        
+        return maxprofit    
+    
